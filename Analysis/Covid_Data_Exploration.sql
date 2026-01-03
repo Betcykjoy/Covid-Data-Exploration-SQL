2cd2567961339fb25951a3f7c83b189424d6ccc1
Betcy Karukamalil Joy <k.betcyjoy@gmail.com>
2026-01-03 08:06:57 -0800

    Add files via upload
    The analysis shows that countries with faster vaccination rollouts experienced a slower growth in death rates over time, highlighting the importance of early vaccine distribution strategies in reducing COVID-19 mortality and healthcare system strain

---
+++ Analysis/Covid_Data_Exploration.sql
@@ -0,0 +1,148 @@
+SELECT *
+FROM CovidDeaths
+WHERE continent IS NOT NULL
+ORDER BY location, date;
+
+//Total Cases vs Total Deaths
+
+SELECT 
+    location,
+    date,
+    total_cases,
+    total_deaths,
+    (total_deaths / total_cases) * 100 AS death_percentage
+FROM CovidDeaths
+WHERE location = 'Canada'
+  AND continent IS NOT NULL
+ORDER BY date;
+
+//Total Cases vs Population
+
+SELECT
+    location,
+    date,
+    population,
+    total_cases,
+    (total_cases / population) * 100 AS percent_population_infected
+FROM CovidDeaths
+WHERE continent IS NOT NULL
+ORDER BY location, date;
+
+//Countries with Highest Infection Rate
+
+SELECT
+    location,
+    population,
+    MAX(total_cases) AS highest_infection_count,
+    MAX((total_cases / population)) * 100 AS percent_population_infected
+FROM CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY location, population
+ORDER BY percent_population_infected DESC;
+
+Countries with Highest Death Count
+SELECT
+    location,
+    MAX(CAST(total_deaths AS INT)) AS total_death_count
+FROM CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY total_death_count DESC;
+
+//Death Count by Continent
+
+SELECT
+    continent,
+    MAX(CAST(total_deaths AS INT)) AS total_death_count
+FROM CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY total_death_count DESC;
+
+//Global Numbers (Daily)
+
+SELECT
+    date,
+    SUM(new_cases) AS total_cases,
+    SUM(new_deaths) AS total_deaths,
+    (SUM(new_deaths) / SUM(new_cases)) * 100 AS death_percentage
+FROM CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY date
+ORDER BY date;
+
+//Total Global Numbers
+
+SELECT
+    SUM(new_cases) AS total_cases,
+    SUM(new_deaths) AS total_deaths,
+    (SUM(new_deaths) / SUM(new_cases)) * 100 AS death_percentage
+FROM CovidDeaths
+WHERE continent IS NOT NULL;
+
+// vaccination table
+
+SELECT *
+FROM CovidVaccinations
+ORDER BY location, date;
+
+
+//Join Deaths & Vaccinations Tables
+
+SELECT
+    d.continent,
+    d.location,
+    d.date,
+    d.population,
+    v.new_vaccinations
+FROM CovidDeaths d
+JOIN CovidVaccinations v
+    ON d.location = v.location
+    AND d.date = v.date
+WHERE d.continent IS NOT NULL
+ORDER BY d.location, d.date;
+
+//Rolling People Vaccinated (Window Function)
+
+SELECT
+    d.continent,
+    d.location,
+    d.date,
+    d.population,
+    v.new_vaccinations,
+    SUM(v.new_vaccinations) 
+        OVER (PARTITION BY d.location ORDER BY d.date) 
+        AS rolling_people_vaccinated
+FROM CovidDeaths d
+JOIN CovidVaccinations v
+    ON d.location = v.location
+    AND d.date = v.date
+WHERE d.continent IS NOT NULL
+ORDER BY d.location, d.date;
+
+// Percentage of Population Vaccinated
+
+WITH PopVsVac AS (
+    SELECT
+        d.continent,
+        d.location,
+        d.date,
+        d.population,
+        v.new_vaccinations,
+        SUM(v.new_vaccinations)
+            OVER (PARTITION BY d.location ORDER BY d.date)
+            AS rolling_people_vaccinated
+    FROM CovidDeaths d
+    JOIN CovidVaccinations v
+        ON d.location = v.location
+        AND d.date = v.date
+    WHERE d.continent IS NOT NULL
+)
+SELECT *,
+       (rolling_people_vaccinated / population) * 100 AS percent_vaccinated
+FROM PopVsVac;
+
+
+
+
+
