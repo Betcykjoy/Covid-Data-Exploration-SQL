2cc7c73bb5a83532872174e4e09602d98b939d58
Betcy Karukamalil Joy <k.betcyjoy@gmail.com>
2026-01-06 07:17:40 -0800

    Refactor SQL queries for Covid data analysis

--- Analysis/Covid_Data_Exploration.sql
+++ Analysis/Covid_Data_Exploration.sql
@@ -1,100 +1,110 @@
+
 SELECT *
-FROM CovidDeaths
+FROM coviddeaths
 WHERE continent IS NOT NULL
 ORDER BY location, date;
 
---Total Cases vs Total Deaths
-SELECT 
+-- Total Cases vs Total Deaths
+
+SELECT coviddeaths
     location,
     date,
     total_cases,
     total_deaths,
     (total_deaths / total_cases) * 100 AS death_percentage
-FROM CovidDeaths
+FROM coviddeaths 
 WHERE location = 'Canada'
   AND continent IS NOT NULL
 ORDER BY date;
 
---Total Cases vs Population
+-- Total Cases vs Population
+
 SELECT
     location,
     date,
     population,
     total_cases,
     (total_cases / population) * 100 AS percent_population_infected
-FROM CovidDeaths
+FROM coviddeaths 
 WHERE continent IS NOT NULL
 ORDER BY location, date;
 
---Countries with Highest Infection Rate
+-- Countries with Highest Infection Rate
+
 SELECT
     location,
     population,
     MAX(total_cases) AS highest_infection_count,
     MAX((total_cases / population)) * 100 AS percent_population_infected
-FROM CovidDeaths
+FROM coviddeaths 
 WHERE continent IS NOT NULL
 GROUP BY location, population
 ORDER BY percent_population_infected DESC;
 
---Countries with Highest Death Count
+-- Countries with Highest Death Count
 SELECT
     location,
     MAX(CAST(total_deaths AS INT)) AS total_death_count
-FROM CovidDeaths
+FROM coviddeaths 
 WHERE continent IS NOT NULL
 GROUP BY location
 ORDER BY total_death_count DESC;
 
---Death Count by Continent
+-- Death Count by Continent
+
 SELECT
     continent,
     MAX(CAST(total_deaths AS INT)) AS total_death_count
-FROM CovidDeaths
+FROM coviddeaths 
 WHERE continent IS NOT NULL
 GROUP BY continent
 ORDER BY total_death_count DESC;
 
---Global Numbers (Daily)
+-- Global Numbers (Daily)
+
 SELECT
     date,
     SUM(new_cases) AS total_cases,
     SUM(new_deaths) AS total_deaths,
     (SUM(new_deaths) / SUM(new_cases)) * 100 AS death_percentage
-FROM CovidDeaths
+FROM coviddeaths 
 WHERE continent IS NOT NULL
 GROUP BY date
 ORDER BY date;
 
---Total Global Numbers
+-- Total Global Numbers
+
 SELECT
     SUM(new_cases) AS total_cases,
     SUM(new_deaths) AS total_deaths,
     (SUM(new_deaths) / SUM(new_cases)) * 100 AS death_percentage
-FROM CovidDeaths
+FROM coviddeaths 
 WHERE continent IS NOT NULL;
 
 -- vaccination table
+
 SELECT *
-FROM CovidVaccinations
+FROM covidvaccinations
 ORDER BY location, date;
 
 
---Join Deaths & Vaccinations Tables
+-- Join Deaths & Vaccinations Tables
+
 SELECT
     d.continent,
     d.location,
     d.date,
     d.population,
     v.new_vaccinations
-FROM CovidDeaths d
-JOIN CovidVaccinations v
+FROM coviddeaths d
+JOIN covidvaccinations v
     ON d.location = v.location
     AND d.date = v.date
 WHERE d.continent IS NOT NULL
 ORDER BY d.location, d.date;
 
---Rolling People Vaccinated (Window Function)
+-- Rolling People Vaccinated (Window Function)
+
 SELECT
     d.continent,
     d.location,
@@ -104,14 +114,15 @@ SELECT
     SUM(v.new_vaccinations) 
         OVER (PARTITION BY d.location ORDER BY d.date) 
         AS rolling_people_vaccinated
-FROM CovidDeaths d
-JOIN CovidVaccinations v
+FROM coviddeaths d
+JOIN covidvaccinations v
     ON d.location = v.location
     AND d.date = v.date
 WHERE d.continent IS NOT NULL
 ORDER BY d.location, d.date;
 
 -- Percentage of Population Vaccinated
+
 WITH PopVsVac AS (
     SELECT
         d.continent,
@@ -122,8 +133,8 @@ WITH PopVsVac AS (
         SUM(v.new_vaccinations)
             OVER (PARTITION BY d.location ORDER BY d.date)
             AS rolling_people_vaccinated
-    FROM CovidDeaths d
-    JOIN CovidVaccinations v
+    FROM coviddeaths  d
+    JOIN covidvaccinations v
         ON d.location = v.location
         AND d.date = v.date
     WHERE d.continent IS NOT NULL
@@ -131,9 +142,3 @@ WITH PopVsVac AS (
 SELECT *,
        (rolling_people_vaccinated / population) * 100 AS percent_vaccinated
 FROM PopVsVac;
-
-
-
-
-
-
